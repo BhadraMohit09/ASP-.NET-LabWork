140b1a4bdda325b14e1b9e9b66dae6e72f8b65ba
Bhadra Mohit <mohitbhadra18@gmail.com>
2024-08-21 11:50:50 +0530

    DataLab#4@MAB

--- Program.cs
+++ Program.cs
@@ -1,67 +1,40 @@
-﻿using Lab3_MAB;
+﻿using Lab_4_MAB;
 using System;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
+
 public class Program
 {
     public static void Main()
     {
-        //Lab3A1 a1 = new Lab3A1();
-        //Lab3A1 a2 = new Lab3A1();
-        //int resultA = a1.Addition(20, 30);
-        //float resultB = a2.Addition(20, 30);
+        //FileA f1 = new FileA();
+        //f1.GetData();
 
-        //Console.WriteLine("Addition of Integers ==> " + resultA);
+        //FileB f2 = new FileB();
+        //f2.GetData();
 
-        //Console.WriteLine("Addition of Floats ==> " + resultB);
+        //FileC f3= new FileC(10);
+        //f3.Push(1);
+        //f3.Push(2);
+        //f3.Push(3);
+        //f3.Push(4);
+        //f3.Push(5);
+        //f3.Display();
+        //f3.Pop();
+        //f3.Display();
 
-        //Lab3A2 a1 = new Lab3A2();
-        //double resultA = a1.Area(5);
-        //double resultB = a1.Area(5,10);
+        FileD f4 = new FileD();
+        f4.EnQueue(1);
+        f4.EnQueue(2);
+        f4.EnQueue(3);
 
-        //Console.WriteLine("Area of Square ==> " + resultA);
+        Console.WriteLine("Queue is:: ");
+        f4.Display();
 
-        //Console.WriteLine("Area of Rectangle ==> " + resultB);
+        Console.WriteLine("Peek:: " + f4.Peek());
 
-        //HDFC h1 = new HDFC();
-        //double resultA = h1.CalculateInterest(10, 20, 30);
+        Console.WriteLine("DeQueue:: " + f4.DeQueue());
 
-        //SBI s1 = new SBI();
-        //double resultB = s1.CalculateInterest(20, 30, 40);
+        Console.WriteLine(f4.Contains(2)); //Returns True
 
-        //ICICI i1 = new ICICI();
-        //double resultC = i1.CalculateInterest(30, 40, 50);
-
-        //Console.WriteLine("Interest Rate for HDFC ==> " + resultA);
-        //Console.WriteLine("Interest Rate for SBI ==> " + resultB);
-        //Console.WriteLine("Interest Rate for ICICI ==> " + resultC);
-
-        //Lab3A4 a4 = new Lab3A4();
-        //a4.Display();
-        //a4.Yellow();
-        //a4.Green();
-        //a4.Red();
-        //a4.Calculate();
-
-        //Apollo a1 = new Apollo();
-        //a1.HospitalDetails();
-
-        //WorkHard w1 = new WorkHard();
-        //w1.HospitalDetails();
-
-        //Gokul g1 = new Gokul();
-        //g1.HospitalDetails();
-
-        //Lab3B6 l6 = new Lab3B6();
-        //int resultA = l6.FindArea(5);
-        //double resultB =  l6.FindArea(4, 5);
-        //double resultC = l6.FindArea(3.14);
-
-        //Console.WriteLine("Area of Square ==> " + resultA);
-
-        //Console.WriteLine("Area of Rectangle ==> " + resultB);
-
-        //Console.WriteLine("Area of Circle ==> " + resultC);
-
+        f4.Clear();
     }
 }
