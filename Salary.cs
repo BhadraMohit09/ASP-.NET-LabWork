e2351970da2c6cd0ad1251a697237f6b10ba99c3
Bhadra Mohit <mohitbhadra18@gmail.com>
2024-08-21 11:47:56 +0530

    DataLab#1@MAB

---
+++ Salary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_MAB
+{
+    internal class Salary
+    {
+        public double Basic;
+        public double TA;
+        public double DA;
+        public double HRA;
+        public Salary(double basic, double ta, double da = 0.1, double hra = 0.2)
+        {
+            Basic = basic;
+            TA = ta;
+            DA = da;
+            HRA = hra;
+        }
+        public double CalculateTotalSalary()
+        {
+            return Basic + (Basic * TA) + (Basic * DA) + (Basic * HRA);
+        }
+    }
+}
